59bfd1719912259957ea25cf0689c97e06d8092f
Sophie Vanderveken <vanderveken.sophie@gmail.com>
2017-09-04 18:07:29 +0100

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var path = require('path');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const extractCSS = new ExtractTextPlugin('[name].bundle.css');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ["es2015"]
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [autoprefixer]
+                        }
+                    }, 'sass-loader', 'resolve-url-loader']
+                })
+            },
+            {
+                test: /\.(ttf|eot|woff2?)(\?v=[a-z0-9=\.]+)?$/i,
+                loader: 'file-loader?name=./fonts/[name].[ext]'
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg|ico)$/i,
+                loaders: 'file-loader?name=./img/[sha512:hash:base64:7].[ext]'
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }
+        ]
+    },
+    plugins: [
+        extractCSS,
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
+};
