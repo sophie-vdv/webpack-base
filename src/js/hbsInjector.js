44ebbcbc8c366a9a3f8f4048d258d9fc4d39f571
Comum <miguel.rib.20@gmail.com>
2017-12-23 12:45:35 +0000

    More work on the about page

--- src/js/hbsInjector.js
+++ src/js/hbsInjector.js
@@ -1,8 +1,53 @@
+let $body = $('body');
+let $container = $body.find('.js-content-area');
+
+const escButton = `
+    <div id="escButtonContainer" class="aboutButton buttonHover no-select-text cursor-pointer js-close-about-area">
+        ESC
+    </div>
+`;
+
+const arrowsContainer = `
+    <div id="arrowsButtonsContainer">
+        <div id="leftButtonArrow" class="aboutButton js-about-left-arrow buttonHover no-select-text cursor-pointer">&#9665;</div>
+        <div id="rightButtonArrow" class="aboutButton js-about-right-arrow buttonHover no-select-text cursor-pointer">&#9655;</div>
+    </div>
+`;
+
+/*
+ * Template example
+ * const beer = {
+    name: 'Belgian Wit',
+    brewery: `Steam Whistle Brewery`,
+    keywords: ['pale', 'cloudy', 'spiced', 'crisp']
+};
+
+function renderKeywords(keywords) {
+    return `
+    <ul>
+        ${keywords.map(keyword => `<li>${keyword}</li>`)}
+    </ul>
+    `;
+}
+
+const markup = `
+<div class="beer">
+    <h2>${beer.name}</h2>
+    <p class="brewery">${beer.brewery}</p>
+    ${renderKeywords(beer.keywords).join('')}
+</div>
+`;
+
+document.body.innerHTML = markup;
+ *
+ */
+
+
 module.exports = {
     addAboutButton: function (buttonContent = '', classPositioning = '') {
-        console.log('Adding buttons');
+        $container.append(escButton);
     },
     addArrowsContainer: function (buttonContent = '', classPositioning = '') {
-        console.log('Adding arrows');
+        $container.append(arrowsContainer);
     }
 }
